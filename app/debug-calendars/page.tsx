49a21dfc64f2127015b738a0768800728eaa8344
Elvis Kvalbergs <elvis.kvalbergs@gmail.com>
2026-03-27 09:45:35 +0200

    debug: force dynamic rendering for /debug-calendars

--- app/debug-calendars/page.tsx
+++ app/debug-calendars/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation'
 import { graphFetch } from '@/lib/graph/client'
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 export default async function DebugCalendarsPage() {
   const session = await auth()
   if (!session) redirect('/login')
